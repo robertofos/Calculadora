c578d20c12215225337b745e4eef6e3f51be6cb8
Roberto Santos <robertofos@gmail.com>
2019-06-01 18:06:56 -0300

    a

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.calculadora;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 7521 ;
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //init providers
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        showSignInOption();
+    }
+
+    private void showSignInOption(){
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.Mytheme)
+                .build(),MY_REQUEST_CODE
+        );
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == MY_REQUEST_CODE) {
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(requestCode==RESULT_OK)
+            {
+                //get User
+                FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();                )
+
+            }
+        }
     }
 }
